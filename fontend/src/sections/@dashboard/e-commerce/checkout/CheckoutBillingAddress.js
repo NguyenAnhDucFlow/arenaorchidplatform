d13b5a8f5f2410b8c1582d83238064df996ba8c2
sangdo2312 <dovansangabc123321@gmail.com>
2024-03-23 23:52:48 +0700

    handle delete address item

--- fontend/src/sections/@dashboard/e-commerce/checkout/CheckoutBillingAddress.js
+++ fontend/src/sections/@dashboard/e-commerce/checkout/CheckoutBillingAddress.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { useState, useContext } from 'react';
 // @mui
-import { Box, Grid, Card, Button, Typography } from '@mui/material';
+import { Box, Grid, Card, Button, Typography, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
 // redux
 import { useDispatch, useSelector } from '../../../../redux/store';
 import { onBackStep, onNextStep, createBilling } from '../../../../redux/slices/product';
@@ -15,6 +15,7 @@ import CheckoutSummary from './CheckoutSummary';
 import CheckoutNewAddressForm from './CheckoutNewAddressForm';
 import { AuthContext } from '../../../../contexts/JWTContext';
 import useAuth from '../../../../hooks/useAuth';
+import axios from '../../../../utils/axios';
 
 // ----------------------------------------------------------------------
 
@@ -48,6 +49,14 @@ export default function CheckoutBillingAddress() {
     dispatch(createBilling(value));
   };
 
+  const handelDeleteBilling = (id) => {
+    console.log('handelDeleteBilling', id);
+    axios.delete(`/shipment/${id}`)
+    .then((res) => {
+      console.log('res', res);
+    })
+  }
+
   return (
     <>
       <Grid container spacing={3}>
@@ -60,6 +69,7 @@ export default function CheckoutBillingAddress() {
               address={address}
               onNextStep={handleNextStep}
               onCreateBilling={handleCreateBilling}
+              onDeleteBilling={handelDeleteBilling}
             />
           ))}
           <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -98,10 +108,20 @@ AddressItem.propTypes = {
   address: PropTypes.object,
   onNextStep: PropTypes.func,
   onCreateBilling: PropTypes.func,
+  onDeleteBilling: PropTypes.func
 };
 
-function AddressItem({ address, onNextStep, onCreateBilling }) {
+function AddressItem({ address, onNextStep, onCreateBilling, onDeleteBilling }) {
   const { receiver, fullAddress, addressType, phone, id } = address;
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const { user } = useAuth();
   const isDefault = user.defaultShipmentId === id;
@@ -110,6 +130,11 @@ function AddressItem({ address, onNextStep, onCreateBilling }) {
     onCreateBilling(address);
     onNextStep();
   };
+
+  const handelDeleteBilling = () => {
+    handleClose();
+    onDeleteBilling(id);
+  }
 
   return (
     <Card sx={{ p: 3, mb: 3, position: 'relative' }}>
@@ -141,7 +166,7 @@ function AddressItem({ address, onNextStep, onCreateBilling }) {
         }}
       >
         {!isDefault && (
-          <Button variant="outlined" size="small" color="inherit">
+          <Button variant="outlined" size="small" color="inherit" onClick={handleClickOpen}>
             Delete
           </Button>
         )}
@@ -150,6 +175,27 @@ function AddressItem({ address, onNextStep, onCreateBilling }) {
           Deliver to this Address
         </Button>
       </Box>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Delete"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you want to delete this Address?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handelDeleteBilling} autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Card>
   );
 }
